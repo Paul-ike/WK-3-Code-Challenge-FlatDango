9cfa59638a8546cacf105c9d2e3fcfad7294ce9e
Paul <paul.ngugi@student.moringaschool.com>
2022-12-11 22:39:41 +0300

    initialize and declared DOM manipulation of html elements

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const movieTitle = document.getElementById('films');
+    const btn = document.getElementById("buy-ticket");
+
+     
+
     
+        
 
 });
