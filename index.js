7715299b86d548dd98f04c2683c98bd6e4b28c01
Paul <paul.ngugi@student.moringaschool.com>
2022-12-11 22:40:43 +0300

    styeld cursor

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const movieTitle = document.getElementById('films');
+    movieTitle.style.cursor = 'pointer';
     const btn = document.getElementById("buy-ticket");
 
      
