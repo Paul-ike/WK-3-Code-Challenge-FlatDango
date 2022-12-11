227868fb7986af4457600ef864f0a5c64390013e
Paul <paul.ngugi@student.moringaschool.com>
2022-12-11 22:50:57 +0300

    DOM rendering our data

--- index.js
+++ index.js
@@ -11,7 +11,29 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch('http://localhost:3000/films')
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            
+            let img = document.getElementById('poster');
+                img.src = `${data[0].poster}`;
+
+            let title = document.getElementById('title');
+                title.innerText = `${data[0].title}`;
+
+            let runtime = document.getElementById('runtime');
+                runtime.innerText = `${data[0].runtime} minutes`;
+
+            let description = document.getElementById('film-info');
+                description.innerText = `${data[0].description}`;
+
+            let showtime = document.getElementById('showtime');
+            showtime.innerText = `${data[0].showtime}`;
+
+            let ticket = document.getElementById('ticket-num');
+                let capacity = `${data[0].capacity}` 
+                let ticketsSold = `${data[0].tickets_sold}`
+                let remainingTickets = +capacity - +ticketsSold;
+                ticket.innerText = remainingTickets;
+
+
             
     })
 
