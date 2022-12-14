1506ac2c6ce137dc2595c29220ed48217e59a682
Paul <paul.ngugi@student.moringaschool.com>
2022-12-14 12:29:35 +0300

    completed code challenge

--- index.js
+++ index.js
@@ -1,65 +1,66 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const listEl = document.querySelector('ul');
-    const element = document.getElementById("buy-ticket");
+    const movieTitle = document.getElementById('films');
+    movieTitle.style.cursor = 'pointer'
+    const btn = document.getElementById("buy-ticket");
 
-    let soldButton = document.createElement("div");
-    soldButton.innerHTML = `
+    let soldBTN = document.createElement("div");
+    soldBTN.innerHTML = `
         <button class= "sold-out">Sold Out</button>
         `;
 
-    soldButton.replaceWith(element);  
+    soldBTN.replaceWith(btn);  
 
     
         fetch('http://localhost:3000/films')
         .then(response => response.json())
         .then(data => {
             
-            let img = document.querySelector('img#poster');
+            let img = document.getElementById('poster');
                 img.src = `${data[0].poster}`;
 
-            let title = document.querySelector('#title');
+            let title = document.getElementById('title');
                 title.innerText = `${data[0].title}`;
 
-            let runtime = document.querySelector('#runtime');
+            let runtime = document.getElementById('runtime');
                 runtime.innerText = `${data[0].runtime} minutes`;
 
-            let description = document.querySelector('#film-info');
+            let description = document.getElementById('film-info');
                 description.innerText = `${data[0].description}`;
 
-            let showtime = document.querySelector('#showtime');
+            let showtime = document.getElementById('showtime');
             showtime.innerText = `${data[0].showtime}`;
 
-            let ticket = document.querySelector('#ticket-num');
+            let ticket = document.getElementById('ticket-num');
                 let capacity = `${data[0].capacity}` 
                 let ticketsSold = `${data[0].tickets_sold}`
                 let remainingTickets = +capacity - +ticketsSold;
                 ticket.innerText = remainingTickets;
 
 
-                let buyTicket = document.querySelector('#buy-ticket');
+                let buyTicket = document.getElementById('buy-ticket');
                 buyTicket.addEventListener('click', () => {
-                     soldButton.replaceWith(element);  
+                     soldBTN.replaceWith(btn);  
                     if (remainingTickets > 0) {
                         remainingTickets -= 1;
                         ticket.innerText = remainingTickets;
                     } else {
                      
-                        element.replaceWith(soldButton);   
+                        btn.replaceWith(soldBTN);   
                     }      
                     
                 })
 
             data.forEach(data =>{
-                listEl.insertAdjacentHTML('beforeend', `<li class="film item">${data.title}</li>`);
+                movieTitle.insertAdjacentHTML('beforeend', `<li class="film item">${data.title}</li>`);
                 
             });
                
-            let titleEl = document.querySelectorAll('.film');
-            for (let index = 0; index < titleEl.length; index++) {
-                let el = titleEl[index];         
+            let titleElement = document.getElementsByClassName('film');
+            for (let index = 0; index < titleElement.length; index++) {
+                let element = titleElement[index];         
                 
-                el.addEventListener('click', () =>{
+                element.addEventListener('click', () =>{
                     
                     img.src = `${data[index].poster}`;
                     title.innerText = `${data[index].title}`;
@@ -71,17 +72,17 @@ document.addEventListener('DOMContentLoaded', () => {
                         let remainingTickets = +capacity - +ticketsSold;
                     ticket.innerText = remainingTickets;
 
-                    soldButton.replaceWith(element);  
+                    soldBTN.replaceWith(btn);  
                    
-                let buyTicket = document.querySelector('#buy-ticket');
+                let buyTicket = document.getElementById('buy-ticket');
                 buyTicket.addEventListener('click', () => {
-                     soldButton.replaceWith(element);  
+                     soldBTN.replaceWith(btn);  
                     if (remainingTickets > 0) {
                         remainingTickets -= 1;
                         ticket.innerText = remainingTickets;
                     } else {
                      
-                        element.replaceWith(soldButton);   
+                        btn.replaceWith(soldBTN);   
                     }      
                     
                 })
